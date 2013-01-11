a9e55572496025dcb02bc7f18457817816fc57f7
Jacky Alciné <me@jalcine.me>
2013-01-11 03:17:56 -0500

    Added in my custom path and shell options.

---
+++ .bash/path.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+#
+
+# Add my personal local path into the system.
+PATH="~/.local/bin:$PATH"
+
+
+# Add Android's tools to the path.
+ANDROID_SDK_PATH="/home/jalcine/Development/References/Resources/SDKs/Android"
+PATH="$ANDROID_SDK_PATH/platform-tools:$PATH"
+PATH="$ANDROID_SDK_PATH/tools:$PATH"
---
+++ .bash/shopt.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+###########################################################
+###########################################################
+## Shell Options
+##
+## @author Jacky Alcine <me@jalcine.me>
+## 01-08-2013 21:07 UTC-05:00
+##
+## Provide a centralized location for tweaking all of the
+## options entered by 'shopt'.
+###########################################################
+###########################################################
+
+# Append all history entries.
+shopt -s histappend
+
+# Resize the window after commands.
+shopt -s checkwinsize
