8d70ad7fc74a6c2bcb81365b51bc7ceb923c843b
Jacky Alciné <me@jalcine.me>
2013-03-01 20:28:21 -0500

    Update variables and add tmuxinator sessions.

---
+++ home/.bash/variables.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+# Some environment variables I'd use a lot.
+
+# Enable the use of ccache.
+export USE_CCACHE=1
+
+# Make vim our default console editor.
+export EDITOR=vim
+
+# We want to see as much as possible, not less.
+export PAGER=most
+
+# Since I <3 KDE, we'd use KDE to manage my keychain.
+export SSH_ASKPASS=ksshaskpass
+
+# Include my local path into the CMake module discovery.
+export CMAKE_PREFIX_PATH="$CMAKE_PREFIX_PATH:$HOME/.local"
+
+# Default the installation to my local installation.
+export CMAKE_INSTALL_PREFIX="$HOME/.local"
+
+# Default the build type to be debugged.
+export CMAKE_BUILD_TYPE="Debug"
+
+# When searching for awesome KDE stuff, include my built versions.
+export KDEDIRS="$KDEDIRS:$HOME/.local"
+
+# When using `pkgconfig`, include my built versions.
+export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$HOME/.local/lib/pkgconfig"
+
+# Include my library's built out.
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.local/lib"
