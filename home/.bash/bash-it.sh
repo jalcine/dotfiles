726b1d39da8ea2aefb3e6d21354be4c6783ec297
Jacky Alciné <me@jalcine.me>
2013-06-18 01:49:41 -0400

    Updated theme used.

--- home/.bash/bash-it.sh
+++ home/.bash/bash-it.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export BASH_IT="$HOME/.bash/bash-it"
-export BASH_IT_THEME='zork'
+export BASH_IT_THEME='pete'
 
 # Don't check mail when opening terminal.
 unset MAILCHECK
@@ -9,7 +9,6 @@ unset MAILCHECK
 # Change this to your console based IRC client of choice.
 export IRC_CLIENT='weechat-curses'
 
-# Set vcprompt executable path for scm advance info in prompt (demula theme)
 export VCPROMPT_EXECUTABLE=~/.local/bin/vcprompt
 
 # Load Bash It
