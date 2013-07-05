db2b42f747435dd6e6cf572d8602b06382e403e0
Jacky Alciné <me@jalcine.me>
2013-07-05 02:50:03 -0400

    Updated paths.

--- home/.bash/path.sh
+++ home/.bash/path.sh
@@ -1,11 +1,5 @@
-#!/usr/bin/bash
-#
+#!/usr/bin/env bash
 
 # Add my personal local path into the system.
-PATH="~/.local/bin:$PATH"
+PATH="$HOME/.local/bin:$PATH"
 
-
-# Add Android's tools to the path.
-ANDROID_SDK_PATH="$HOME/Development/References/Resources/SDKs/Android"
-PATH="$ANDROID_SDK_PATH/platform-tools:$PATH"
-PATH="$ANDROID_SDK_PATH/tools:$PATH"
