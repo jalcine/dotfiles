92e937082ce7f6f6870a1320ba0a2cfba54d49e0
Jacky Alciné <me@jalcine.me>
2013-07-07 15:18:57 -0400

    Changed options for bash-it.
      * Changed IRC object detection.
      * Switched default theme.

--- home/.bash/bash-it.sh
+++ home/.bash/bash-it.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 export BASH_IT="$HOME/.bash/bash-it"
-export BASH_IT_THEME='pete'
+export BASH_IT_THEME='jalcine'
 
 # Don't check mail when opening terminal.
 unset MAILCHECK
 
 # Change this to your console based IRC client of choice.
-[ -f `which weechat-curses` ] && export IRC_CLIENT='weechat-curses';
+export IRC_CLIENT='weechat-curses'
 
 # Define the VC prompt.
 export VCPROMPT_EXECUTABLE=$HOME/.local/bin/vcprompt
