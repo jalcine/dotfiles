16179e4f31d3580536f3e9205ab3172341729341
Jacky Alciné <me@jalcine.me>
2013-07-07 04:50:35 -0400

    Updated globalvars.

--- home/.bash/variables.sh
+++ home/.bash/variables.sh
@@ -11,7 +11,7 @@ export EDITOR=vim
 export PAGER=most
 
 # Since I <3 KDE, we'd use KDE to manage my keychain.
-export SSH_ASKPASS=ksshaskpass
+[ -d "$HOME/.kde" ] && export SSH_ASKPASS=ksshaskpass
 
 # Include my local path into the CMake module discovery.
 export CMAKE_PREFIX_PATH="$HOME/.local:$CMAKE_PREFIX_PATH"
@@ -28,12 +28,10 @@ export CMAKE_BUILD_TYPE="Debug"
 export LIBRARY_PATH="$HOME/.local/lib:$LIBRARY_PATH"
 
 # When searching for awesome KDE stuff, include my built versions.
-export KDEDIRS="$HOME/.local:$KDEDIRS"
+[ -d "$HOME/.kde"] && export KDEDIRS="$HOME/.local:$KDEDIRS";
 
 # When using `pkgconfig`, include my built versions.
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH:$HOME/.local/lib/pkgconfig"
 
 # Include my library's built out.
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/.local/lib"
-
-$HOME/.kde/Autostart/AddSshKeys.sh
