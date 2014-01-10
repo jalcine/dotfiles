f1033700677aebe80bd65bebcb40fb00f410e469
Jacky Alciné <me@jalcine.me>
2014-01-10 07:16:48 -0500

    Cleaned up shop.

--- home/.bash/config.sh
+++ home/.bash/config.sh
@@ -8,35 +8,36 @@
 ## Jacky Alcine's root personal Bash configuration.
 ###########################################################
 
-###########################################################
-## Miscellaneous
-###########################################################
+# First, first that mapping.
+[ -e xmodmap ] && xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
+
 # If not running interactively, don't do anything.
 [ -z "$PS1" ] && return
+
 # Give me a fortune if I'm not in tmux.
 [ -z $TMUX ] && [ -e /usr/bin/fortune ] && fortune -e
-###########################################################
-## History 
-###########################################################
+
 # Keep a relative short immediate history.
 HISTSIZE=8
+
 # Keep the core history file quite large.
 HISTFILESIZE=42949673
+
 # Ignore duplicates and whitespace.
 HISTCONTROL=ignoredups:ignorespace
+
 # Use UTF-8 encoding.
 LC_ALL=en_US.UTF-8
+
 # Speak English (I have French installed as well).
 LANGUAGE=en_US.UTF-8
+
 # Update time zone.
 TZ="America/New_York"
+
 # Export data.
 export HISTSIZE HISTFILESIZE HISTCONTROL LC_ALL TZ LANGUAGE
 
-###########################################################
-## Modules
-###########################################################
-# Load the system's completions.
 . $HOME/.bash/shopt.sh
 . $HOME/.bash/variables.sh
 . $HOME/.bash/bash-it.sh
