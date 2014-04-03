2658da9ce8cdc89c092120bbf26172c3e8ca6d5a
Jacky Alciné <me@jalcine.me>
2014-04-03 12:25:40 -0400

    Corrupted index; might have to force-push.

--- home/.bash/bash-it.sh
+++ home/.bash/bash-it.sh
@@ -2,6 +2,8 @@
 
 export BASH_IT="$HOME/.bash/bash-it"
 export BASH_IT_THEME='jalcine'
+export JALCINE_THEME_USER="jacky"
+export JALCINE_THEME_HOME="neuromancer"
 
 # Don't check mail when opening terminal.
 unset MAILCHECK
