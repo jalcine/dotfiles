479dc4cbfcfe1da417ad861e1286d0516f44080d
Jacky Alciné <me@jalcine.me>
2014-10-17 18:39:42 -0400

    Updated weechat and rbenv.

--- home/.bash/bash-it.sh
+++ home/.bash/bash-it.sh
@@ -2,8 +2,8 @@
 
 export BASH_IT="$HOME/.bash/bash-it"
 export BASH_IT_THEME="jalcine"
-export JALCINE_THEME_USER="jacky"
-export JALCINE_THEME_HOME="neuromancer.jalcine"
+export JALCINE_THEME_USER="jalcine"
+export JALCINE_THEME_HOME="stark.jalcine"
 
 # Don't check mail when opening terminal.
 unset MAILCHECK
