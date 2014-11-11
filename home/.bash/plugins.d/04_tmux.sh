d34bb16e9ace6daa600bf8cd3ae74d7275f5947d
Jacky Alciné <jalcine@shutterstock.com>
2014-11-11 11:00:09 -0500

    [patch] Add support for handling new SSH sessions with tmux.
      This has been a bitch in my side for so long. TODAY, YOU DIE.

---
+++ home/.bash/plugins.d/04_tmux.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# vim: set fdm=marker :
+# =========================================================================== #
+# File: ~/.bash/plugins.d/tmux.sh
+# Author: Jacky Alcine <me@jalcine.me>
+# Description: Making tmux easier to use.
+# =========================================================================== #
+
+# {{{ SSH Forwarding
+SOCK="/tmp/ssh-agent-$USER-tmux"
+
+if test $SSH_AUTH_SOCK && [ $SSH_AUTH_SOCK != $SOCK ]
+then
+    rm -f /tmp/ssh-agent-$USER-screen
+    ln -sf $SSH_AUTH_SOCK $SOCK
+    export SSH_AUTH_SOCK=$SOCK
+fi
+# }}}
