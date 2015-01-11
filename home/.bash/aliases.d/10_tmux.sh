d50c158f2d3547d5eee48acc320e2fde07766118
Jacky Alcine <me@jalcine.me>
2015-01-11 09:42:57 -0500

    add aliases for tmux.

---
+++ home/.bash/aliases.d/10_tmux.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# =========================================================================== #
+# File: ~/.bash/aliases.d/99_tmux.sh
+# Author: Jacky Alcine <me@jalcine.me>
+# Description: Aliases for tmux.
+# =========================================================================== #
+
+alias tmuxa="tmux attach -t"
+alias tmuxl="tmux detach-session"
+alias tmuxk="tmux kill-session"
+alias tmuxK="tmux kill-server"
