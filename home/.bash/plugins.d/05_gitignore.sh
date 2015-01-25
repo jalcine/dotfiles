327dd782fbee3eca735e799fa22562fef4758a39
Jacky Alcine <me@jalcine.me>
2015-01-25 08:58:01 -0500

    add plugin to download git ignores.

---
+++ home/.bash/plugins.d/05_gitignore.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# =========================================================================== #
+# File: ~/.bash/plugins.d/05_gitignore.sh
+# Author: Jacky Alcine <me@jalcine.me>
+# Description: Git ignore downloader.;
+# =========================================================================== #
+
+function gi() {
+  curl -L -s https://www.gitignore.io/api/\$@ ;
+}
