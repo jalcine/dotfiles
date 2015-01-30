c8406bf45c9f571772ef023554cf65bc735ec642
Jacky Alcine <me@jalcine.me>
2015-01-30 10:59:54 -0500

    add aliases and plugins for npm.

---
+++ home/.bash/aliases.d/11_npm.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# =========================================================================== #
+# File: ~/.bash/aliases.d/11_npm.sh
+# Author: Jacky Alcine <me@jalcine.me>
+# Description: Aliases for tmux.
+# =========================================================================== #
+
+alias ni="npm install"
+alias nt="npm test"
+alias ns="npm start"
