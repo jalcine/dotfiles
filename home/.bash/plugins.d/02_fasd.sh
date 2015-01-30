ef26824f0dfad3892ea5052afbb030b72e2b5be7
Jacky Alcine <me@jalcine.me>
2015-01-30 10:05:10 -0500

    patched fasd, used haramonic16 as theme.

--- home/.bash/plugins.d/02_fasd.sh
+++ home/.bash/plugins.d/02_fasd.sh
@@ -8,4 +8,13 @@
 # Eval our friend into the shell.
 eval "$(fasd --init bash-hook)";
 
-export PROMPT_COMMAND="$PROMPT_COMMAND;_fasd_prompt_func";
+case $PROMPT_COMMAND in
+  *_fasd_prompt_func*)
+    ;;
+  *\;*)
+    PROMPT_COMMAND="_fasd_prompt_func;$PROMPT_COMMAND"
+    ;;
+  "")
+    PROMPT_COMMAND="_fasd_prompt_func"
+    ;;
+esac
