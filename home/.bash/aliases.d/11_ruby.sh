9f9cfe9f0ae7590fef9a64917e9184f208f4b63d
Jacky Alciné <me@jalcine.me>
2015-02-04 23:03:31 -0500

    Add some aliases for bundler + ruby.

---
+++ home/.bash/aliases.d/11_ruby.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# =========================================================================== #
+# File: ~/.bash/aliases.d/11_ruby.sh
+# Author: Jacky Alcine <me@jalcine.me>
+# Description: Aliases for rubby (not a typo).
+# =========================================================================== #
+
+alias be="bundle exec"
+alias bi="bundle install"
