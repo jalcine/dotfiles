ab8ed7c84cc61dc991413651898d623f95481b82
Jacky Alciné <jacky@getclef.com>
2016-02-12 11:24:21 -0800

    fix(alias): Chop down size for Vagrant.

---
+++ home/.bash/aliases.d/12_vagrant.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# =========================================================================== #
+# File: ~/.bash/aliases.d/13_vagrant.sh
+# Author: Jacky Alcine <yo@jacky.wtf>
+# Description: Aliases for Vagrant.
+# =========================================================================== #
+
+alias vgs="vagrant global-status"
