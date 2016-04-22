0c535a9726c6ab2e66ccfe0dc37ebb528da22c63
Jacky Alciné <yo@jacky.wtf>
2016-04-22 09:00:43 -0700

    use this.

--- home/.bash/plugins.d/10_base16.sh
+++ home/.bash/plugins.d/10_base16.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-BASE16_SHELL="$HOME/.config/base16-shell/base16-default.dark.sh"
-[[ -s $BASE16_SHELL ]] && source $BASE16_SHELL
+BASE16_SHELL="/home/jacky/.config/base16-shell/base16-tomorrow.dark.sh"
+[[ -s $BASE16_SHELL ]] && source "$BASE16_SHELL"
