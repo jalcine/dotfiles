f142faff90207a9399019e7e3ca8177619446e78
Jacky Alciné <jacky@getclef.com>
2016-07-25 15:24:49 -0700

    chore(rm): Remove unnecessary files.

--- home/.bash/plugins.d/01_gvm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# =========================================================================== #
-# File: ~/.bash/plugins.d/gvm.sh
-# Author: Jacky Alcine <yo@jacky.wtf>
-# Description: Set up for gvm
-# =========================================================================== #
-
-[ -s "$HOME/.gvm/scripts/gvm" ] && . "$HOME/.gvm/scripts/gvm"
