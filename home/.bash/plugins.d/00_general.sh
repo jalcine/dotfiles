6ff6161446c373bd37aa530a96919d28d92dde56
Jacky Alciné <jalcine@lyft.com>
2017-04-17 16:45:58 -0700

    More jazz.

--- home/.bash/plugins.d/00_general.sh
+++ home/.bash/plugins.d/00_general.sh
@@ -6,11 +6,11 @@
 # =========================================================================== #
 
 function mkcd {
-  if [[ ! -x $1 ]]; then
-    mkdir -p $1;
+  if [[ ! -x "$1" ]]; then
+    mkdir -p "$1";
   fi
 
-  cd $1;
+  cd "$1" || exit;
 }
 
 function ipinfo {
@@ -18,5 +18,19 @@ function ipinfo {
 }
 
 function resrc {
+  # shellcheck disable=SC1090
   source ~/.bashrc
 }
+
+function codi() {
+ local syntax="${1:-python}"
+ shift
+ nvim -c \
+   "let g:startify_disable_at_vimenter = 1 |\
+   set bt=nofile ls=0 noru nonu nornu |\
+   hi ColorColumn ctermbg=NONE |\
+   hi VertSplit ctermbg=NONE |\
+   hi NonText ctermfg=0 |\
+   Codi $syntax" "$@"
+}
+
