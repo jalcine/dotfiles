a234378eead1099ce082180c98f1bdcad69e650a
Jacky Alciné <yo@jacky.wtf>
2017-09-28 20:54:08 -0700

    Drop some stuff.

--- home/.bash/plugins.d/00_general.sh
+++ home/.bash/plugins.d/00_general.sh
@@ -21,16 +21,3 @@ function resrc {
   # shellcheck disable=SC1090
   source ~/.bashrc
 }
-
-function codi() {
- local syntax="${1:-python}"
- shift
- nvim -c \
-   "let g:startify_disable_at_vimenter = 1 |\
-   set bt=nofile ls=0 noru nonu nornu |\
-   hi ColorColumn ctermbg=NONE |\
-   hi VertSplit ctermbg=NONE |\
-   hi NonText ctermfg=0 |\
-   Codi $syntax" "$@"
-}
-
