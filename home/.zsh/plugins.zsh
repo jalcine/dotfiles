c87fc00d233f57afdfe7fec66d4277b1ecb0e325
Jacky Alciné <jacky@lob.com>
2018-05-22 10:39:50 -0700

    some more tweaks

--- home/.zsh/plugins.zsh
+++ home/.zsh/plugins.zsh
@@ -1,39 +1,24 @@
 zplug "zplug/zplug", hook-build:"zplug --self-manage"
 zplug "plugins/git", from:oh-my-zsh
 zplug "plugins/git-flow", from:oh-my-zsh
-# zplug "plugins/github", from:oh-my-zsh
 zplug "plugins/docker-compose", from:oh-my-zsh
-# zplug "plugins/cpanm", from:oh-my-zsh
-# zplug "plugins/rust", from:oh-my-zsh
-# zplug "plugins/vagrant", from:oh-my-zsh
+zplug "plugins/vagrant", from:oh-my-zsh
 zplug "plugins/pass", from:oh-my-zsh
-# zplug "plugins/rake-fast", from:oh-my-zsh
+zplug "plugins/rake-fast", from:oh-my-zsh
 zplug "plugins/dotenv", from:oh-my-zsh
 zplug "plugins/fasd", from:oh-my-zsh
-# zplug "plugins/jsontools", from:oh-my-zsh
-# zplug "plugins/cp", from:oh-my-zsh
-zplug "plugins/tmux", from:oh-my-zsh
 zplug "plugins/vi-mode", from:oh-my-zsh
 zplug "plugins/common-aliases", from:oh-my-zsh
-# zplug "plugins/adb", from:oh-my-zsh
-# zplug "plugins/heroku", from:oh-my-zsh
-# zplug "plugins/composer", from:oh-my-zsh
-# zplug "plugins/mix-fast", from:oh-my-zsh
+zplug "plugins/mix-fast", from:oh-my-zsh
 zplug "plugins/extract", from:oh-my-zsh
 zplug "tcnksm/docker-alias", use:zshrc
 zplug "chrissicool/zsh-256color", defer:3
-# zplug "sparsick/ansible-zsh"
-# zplug "sroze/docker-compose-zsh-plugin"
-# zplug "b4b4r07/emoji-cli"
-zplug "Seinh/git-prune"
-# zplug "oldratlee/hacker-quotes"
-# zplug "marzocchi/zsh-notify"
-zplug "lukechilds/zsh-better-npm-completion", defer:2
+zplug "oldratlee/hacker-quotes"
+zplug "zsh-users/zsh-autosuggestions"
+zplug "marzocchi/zsh-notify"
 zplug "wbingli/zsh-wakatime"
-zplug "gangleri/pipenv"
 zplug "andrewferrier/fzf-z"
 zplug "chriskempson/base16-shell"
-# zplug "k4rthik/git-cal"
 zplug "paulmelnikow/zsh-startup-timer"
 zplug "bric3/nice-exit-code"
 zplug "stedolan/jq", \
