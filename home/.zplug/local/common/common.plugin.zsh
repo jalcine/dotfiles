b2dd92075352f5214857fb7dc7c2fc2225898305
Jacky Alciné <yo@jacky.wtf>
2018-05-22 10:37:41 -0700

    more mental fixes.

--- home/.zplug/local/common/common.plugin.zsh
+++ home/.zplug/local/common/common.plugin.zsh
@@ -1,5 +1,12 @@
 alias mk='make'
 alias _='sudo'
 alias irc='WEECHAT_PASSPHRASE=$(pass Applications/Weechat) weechat'
+alias ..='cd ..'
+alias ...='cd ../..'
 
 export PATH=$HOME/.local/bin:$PATH
+
+mkcd () {
+  mkdir -p "${1}";
+  cd "${1}";
+}
