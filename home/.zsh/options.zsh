020b974750a4e4523714608fbaf1484de4287913
Jacky Alciné <yo@jacky.wtf>
2018-06-07 22:34:53 -0700

    use nerd font

--- home/.zsh/options.zsh
+++ home/.zsh/options.zsh
@@ -4,8 +4,7 @@ export HISTSIZE=16384
 export SAVEHIST=1024
 export ZSH_CUSTOM=$HOME/.zsh/custom
 export ALIEN_THEME="blue"
-export PLIB_GIT_PUSH_SYM=↑
-export PLIB_GIT_PULL_SYM=↓
+export USE_NERD_FONT=1
 
 setopt appendhistory beep notify extendedglob
 unsetopt autocd
